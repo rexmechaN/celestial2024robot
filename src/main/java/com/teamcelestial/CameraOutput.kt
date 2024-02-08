3ea728222ef698ff868bd336b7c00236a5f0b412
Cem Baykara <cm0bykr@gmail.com>
2024-02-08 17:28:03 +0300

    Add CameraOutput

---
+++ src/main/java/com/teamcelestial/CameraOutput.kt
@@ -0,0 +1,13 @@
+package com.teamcelestial
+
+import edu.wpi.first.math.geometry.Transform3d
+import org.photonvision.PhotonCamera
+
+class CameraOutput(cameraNickname: String?) {
+    private val camera = PhotonCamera(cameraNickname)
+    val bestTarget: Transform3d?
+        get() = camera.latestResult.bestTarget?.bestCameraToTarget
+
+    val allTargets: List<Transform3d>
+        get() = camera.latestResult.getTargets().map { it.bestCameraToTarget }
+}
