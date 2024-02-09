34cfadc3b72e618f223140b5d5ea4ec54687282f
Cem Baykara <cm0bykr@gmail.com>
2024-02-09 16:32:48 +0300

    Update NetworkValue

--- src/main/java/com/teamcelestial/network/NetworkValue.kt
+++ src/main/java/com/teamcelestial/network/NetworkValue.kt
@@ -2,22 +2,22 @@ package com.teamcelestial.network
 
 import edu.wpi.first.networktables.GenericSubscriber
 import edu.wpi.first.networktables.NetworkTableInstance
-import edu.wpi.first.networktables.NetworkTableValue
 
 class NetworkValue<T>(
     valueId: String,
-    valueTypeString: String,
+    networkValueType: NetworkValueType,
     initialValue: T?
 ) {
     private val networkTableInstance = NetworkTableInstance.getDefault()
     private val topic = networkTableInstance.getTopic("/datatable/$valueId")
-    private val subscription: GenericSubscriber = topic.genericSubscribe(valueTypeString)
-    private val publisher = topic.genericPublish(valueTypeString)
+    private val subscription: GenericSubscriber = topic.genericSubscribe(networkValueType.typeName)
+    private val publisher = topic.genericPublish(networkValueType.typeName)
 
     init {
-        if(initialValue != null) publisher.set(initialValue as NetworkTableValue)
+        if(initialValue != null) publisher.setValue(initialValue)
     }
 
+    @Suppress("UNCHECKED_CAST")
     val value: T
         get() = subscription.get().value as T
 
---
+++ src/main/java/com/teamcelestial/network/NetworkValueType.kt
@@ -0,0 +1,9 @@
+package com.teamcelestial.network
+
+enum class NetworkValueType(
+    val typeName: String
+) {
+    kString("string"),
+    kDouble("double"),
+    kBoolean("boolean")
+}
