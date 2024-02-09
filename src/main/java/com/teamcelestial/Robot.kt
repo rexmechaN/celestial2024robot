3005042e65db874308f286ee34c62dc219bcd1b8
Cem Baykara <cm0bykr@gmail.com>
2024-02-09 17:18:03 +0300

    Refactor Robot.java, update NetworkValue.kt

--- src/main/java/com/teamcelestial/Robot.kt
+++ src/main/java/com/teamcelestial/Robot.kt
@@ -1,19 +1,19 @@
-package com.teamcelestial;
+package com.teamcelestial
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.REVLibError;
-import com.revrobotics.SparkPIDController;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.networktables.*;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import com.revrobotics.*
+import com.teamcelestial.network.NetworkValue
+import com.teamcelestial.network.NetworkValueType
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.networktables.DoublePublisher
+import edu.wpi.first.networktables.DoubleSubscriber
+import edu.wpi.first.networktables.DoubleTopic
+import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.TimedRobot
+import kotlin.math.max
+import kotlin.math.min
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Robot extends TimedRobot
-{
+class Robot : TimedRobot() {
     /*private final WPI_VictorSPX leftSlave = new WPI_VictorSPX(4);
     private final WPI_VictorSPX leftMaster = new WPI_VictorSPX(2);
     private final WPI_VictorSPX rightSlave = new WPI_VictorSPX(7);
@@ -24,161 +24,160 @@ public class Robot extends TimedRobot
     private final DifferentialDrive yagsDrive = new DifferentialDrive(leftGroup, rightGroup);
 
     private final SlewRateLimiter limiter = new SlewRateLimiter(0.8);*/
+    var joystick: Joystick = Joystick(0)
 
-    Joystick joystick = new Joystick(0);
+    private val leftCim: CANSparkMax = CANSparkMax(15, CANSparkLowLevel.MotorType.kBrushless)
+    private val rightCim: CANSparkMax = CANSparkMax(16, CANSparkLowLevel.MotorType.kBrushless)
 
-    private final CANSparkMax leftCim = new CANSparkMax(15, CANSparkMax.MotorType.kBrushless);
-    private final CANSparkMax rightCim = new CANSparkMax(16, CANSparkMax.MotorType.kBrushless);
+    private val networkTable: NetworkTableInstance = NetworkTableInstance.getDefault()
+    private val desiredMotorSpeedTopic: DoubleTopic = networkTable.getDoubleTopic("/datatable/motorSpeed")
+    private val desiredMotorSpeedSubscription: DoubleSubscriber = desiredMotorSpeedTopic.subscribe(0.0)
+    private val desiredMotorSpeedPublisher: DoublePublisher = desiredMotorSpeedTopic.publish()
 
-    private final NetworkTableInstance networkTable = NetworkTableInstance.getDefault();
-    private final DoubleTopic desiredMotorSpeedTopic = networkTable.getDoubleTopic("/datatable/motorSpeed");
-    private final DoubleSubscriber desiredMotorSpeedSubscription = desiredMotorSpeedTopic.subscribe(0);
-    private final DoublePublisher desiredMotorSpeedPublisher = desiredMotorSpeedTopic.publish();
+    private var lastRpmPublish: Long = 0
+    private val multiplier = 0.0
 
-    private long lastRpmPublish = 0;
-    private double multiplier = 0.0;
+    private lateinit var leftPid: SparkPIDController
+    private lateinit var rightPid: SparkPIDController
+    private lateinit var driveTrainPid: PIDController
 
-    private SparkPIDController leftPid;
-    private SparkPIDController rightPid;
+    var pValue: NetworkValue<Double> = NetworkValue("P", NetworkValueType.kDouble, 0.0)
+    var iValue: NetworkValue<Double> = NetworkValue("I", NetworkValueType.kDouble, 0.0)
+    var dValue: NetworkValue<Double> = NetworkValue("D", NetworkValueType.kDouble, 0.0)
 
-    private double rpmTarget = 0;
+    private var rpmTarget = 0.0
 
-    private Shooter shooter;
+    private var shooter: Shooter? = null
 
-    @Override
-    public void robotInit()
-    {
-        desiredMotorSpeedPublisher.setDefault(0);
-        desiredMotorSpeedPublisher.set(0);
+    override fun robotInit() {
+        desiredMotorSpeedPublisher.setDefault(0.0)
+        desiredMotorSpeedPublisher.set(0.0)
 
-        leftPid = leftCim.getPIDController();
-        rightPid = rightCim.getPIDController();
+        leftPid = leftCim.pidController
+        rightPid = rightCim.pidController
 
-        List<SparkPIDController> controllerList = new ArrayList<>();
-        controllerList.add(leftPid);
-        controllerList.add(rightPid);
+        shooter = Shooter(listOf(leftPid, rightPid))
 
-        shooter = new Shooter(controllerList);
+        updateDriveTrainPIDController()
 
-        setupPid(leftPid);
-        setupPid(rightPid);
-    }
-
-    void setupPid(SparkPIDController controller) {
-        System.out.println("Motor PID:");
-        System.out.println("P: " + controller.getP());
-        System.out.println("I: " + controller.getI());
-        System.out.println("D: " + controller.getD());
-        System.out.println("F: " + controller.getFF());
-        System.out.println("Iacc: " + controller.getIAccum());
-        System.out.println("======");
-        double kP = 0.000000039;
-        double kI = 0.00000029;
-        double kD = 0;
-        double kIz = 0;
-        double kFF = 0;
-        double kMaxOutput = 1.0;
-        double kMinOutput = -1.0;
-        double maxRPM = 5700;
-        // set PID coefficients
-        controller.setP(kP);
-        controller.setI(kI);
-        controller.setD(kD);
-        controller.setIZone(kIz);
-        controller.setFF(kFF);
-        controller.setOutputRange(kMinOutput, kMaxOutput);
-    }
-
-    @Override
-    public void robotPeriodic() {}
-
-    @Override
-    public void autonomousInit() {}
-
-    @Override
-    public void autonomousPeriodic() {}
-
-    @Override
-    public void teleopInit() {}
-
-    private boolean increaseLatch = false;
-    private boolean reduceLatch = false;
-    private boolean shooterStarted = false;
-    @Override
-    public void teleopPeriodic() {
-        boolean increase = joystick.getRawButton(5);
-        boolean reduce = joystick.getRawButton(3);
-        boolean reverse = joystick.getRawButton(12);
-
-        if(increase && reduce) {
-            rpmTarget = 0;
-        } else if(increase && !increaseLatch) {
-            rpmTarget = rpmTarget + 500;
-        } else if(reduce && !reduceLatch) {
-            rpmTarget = rpmTarget - 500;
+        listOf(pValue, iValue, dValue).forEach {
+            it.setListener {
+                updateDriveTrainPIDController()
+            }
         }
 
-        increaseLatch = increase;
-        reduceLatch = reduce;
+        setupPid(leftPid)
+        setupPid(rightPid)
+    }
 
-        rpmTarget = Math.min(Math.max(0, rpmTarget), 5500);
+    private fun updateDriveTrainPIDController() {
+        driveTrainPid = PIDController(pValue.value, iValue.value, dValue.value)
+    }
 
-        if(joystick.getRawButton(1) || reverse) {
-            if(!shooterStarted) {
-                shooter.start(2.65, 3.40);
-                shooterStarted = true;
+    private fun setupPid(controller: SparkPIDController?) {
+        println("Motor PID:")
+        println("P: " + controller!!.p)
+        println("I: " + controller.i)
+        println("D: " + controller.d)
+        println("F: " + controller.ff)
+        println("Iacc: " + controller.iAccum)
+        println("======")
+        val kP = 0.000000039
+        val kI = 0.00000029
+        val kD = 0.0
+        val kIz = 0.0
+        val kFF = 0.0
+        val kMaxOutput = 1.0
+        val kMinOutput = -1.0
+        val maxRPM = 5700.0
+        // set PID coefficients
+        controller.setP(kP)
+        controller.setI(kI)
+        controller.setD(kD)
+        controller.setIZone(kIz)
+        controller.setFF(kFF)
+        controller.setOutputRange(kMinOutput, kMaxOutput)
+    }
+
+    override fun robotPeriodic() {}
+
+    override fun autonomousInit() {}
+
+    override fun autonomousPeriodic() {}
+
+    override fun teleopInit() {}
+
+    private var increaseLatch = false
+    private var reduceLatch = false
+    private var shooterStarted = false
+    override fun teleopPeriodic() {
+        val increase = joystick.getRawButton(5)
+        val reduce = joystick.getRawButton(3)
+        val reverse = joystick.getRawButton(12)
+
+        if (increase && reduce) {
+            rpmTarget = 0.0
+        } else if (increase && !increaseLatch) {
+            rpmTarget = rpmTarget + 500
+        } else if (reduce && !reduceLatch) {
+            rpmTarget = rpmTarget - 500
+        }
+
+        increaseLatch = increase
+        reduceLatch = reduce
+
+        rpmTarget = min(max(0.0, rpmTarget), 5500.0)
+
+        if (joystick.getRawButton(1) || reverse) {
+            if (!shooterStarted) {
+                shooter!!.start(2.65, 3.40)
+                shooterStarted = true
             }
             //setMotor(reverse ? -rpmTarget : rpmTarget);
         } else {
-            if(shooterStarted) {
-                shooter.stop();
-                shooterStarted = false;
+            if (shooterStarted) {
+                shooter!!.stop()
+                shooterStarted = false
             }
         }
 
-        shooter.tick();
+        shooter!!.tick()
 
-        double leftRpm = leftCim.getEncoder().getVelocity();
-        double rightRpm = rightCim.getEncoder().getVelocity();
+        val leftRpm = leftCim.encoder.velocity
+        val rightRpm = rightCim.encoder.velocity
 
-        if(lastRpmPublish + 500 < System.currentTimeMillis()) {
-            lastRpmPublish = System.currentTimeMillis();
+        if (lastRpmPublish + 500 < System.currentTimeMillis()) {
+            lastRpmPublish = System.currentTimeMillis()
             //System.out.println("Left RPM: " + leftRpm + " \tRight RPM: " + rightRpm + " \tTarget RPM: " + rpmTarget);
         }
     }
 
-    double reference = -1;
+    var reference: Double = -1.0
 
-    void setMotor(double rpm) {
-        if(true) return;
-        if(reference == rpm) return;
-        reference = rpm;
-        System.out.println("Set motor: " + rpm);
-        REVLibError lErr = leftPid.setReference(rpm, CANSparkMax.ControlType.kVelocity);
-        REVLibError rErr = rightPid.setReference(rpm, CANSparkMax.ControlType.kVelocity);
-        if(lErr != REVLibError.kOk) {
-            System.out.println("L REVERROR: " + lErr.name());
+    fun setMotor(rpm: Double) {
+        if (true) return
+        if (reference == rpm) return
+        reference = rpm
+        println("Set motor: $rpm")
+        val lErr: REVLibError = leftPid.setReference(rpm, CANSparkBase.ControlType.kVelocity)
+        val rErr: REVLibError = rightPid.setReference(rpm, CANSparkBase.ControlType.kVelocity)
+        if (lErr != REVLibError.kOk) {
+            println("L REVERROR: " + lErr.name)
         }
-        if(rErr != REVLibError.kOk) {
-            System.out.println("R REVERROR: " + rErr.name());
+        if (rErr != REVLibError.kOk) {
+            println("R REVERROR: " + rErr.name)
         }
     }
 
-    @Override
-    public void disabledInit() {}
+    override fun disabledInit() {}
 
-    @Override
-    public void disabledPeriodic() {}
+    override fun disabledPeriodic() {}
 
-    @Override
-    public void testInit() {}
+    override fun testInit() {}
 
-    @Override
-    public void testPeriodic() {}
+    override fun testPeriodic() {}
 
-    @Override
-    public void simulationInit() {}
+    override fun simulationInit() {}
 
-    @Override
-    public void simulationPeriodic() {}
+    override fun simulationPeriodic() {}
 }
--- src/main/java/com/teamcelestial/network/NetworkValue.kt
+++ src/main/java/com/teamcelestial/network/NetworkValue.kt
@@ -1,25 +1,41 @@
 package com.teamcelestial.network
 
 import edu.wpi.first.networktables.GenericSubscriber
+import edu.wpi.first.networktables.NetworkTableEvent
 import edu.wpi.first.networktables.NetworkTableInstance
+import java.util.*
+import java.util.concurrent.atomic.AtomicReference
 
 class NetworkValue<T>(
     valueId: String,
     networkValueType: NetworkValueType,
     initialValue: T?
 ) {
+    private var listener: ((T) -> Unit)? = null
     private val networkTableInstance = NetworkTableInstance.getDefault()
     private val topic = networkTableInstance.getTopic("/datatable/$valueId")
     private val subscription: GenericSubscriber = topic.genericSubscribe(networkValueType.typeName)
     private val publisher = topic.genericPublish(networkValueType.typeName)
 
+    private val valReference = AtomicReference<T>()
+
     init {
         if(initialValue != null) publisher.setValue(initialValue)
+        networkTableInstance.addListener(subscription, EnumSet.of(NetworkTableEvent.Kind.kValueAll)) {
+            @Suppress("UNCHECKED_CAST")
+            (it.valueData.value.value as T).let { value ->
+                valReference.set(value)
+                listener?.invoke(value)
+            }
+        }
     }
 
-    @Suppress("UNCHECKED_CAST")
     val value: T
-        get() = subscription.get().value as T
+        get() = valReference.get()
+
+    fun setListener(listener: ((T) -> Unit)?) {
+        this.listener = listener
+    }
 
     fun close() {
         subscription.close()
