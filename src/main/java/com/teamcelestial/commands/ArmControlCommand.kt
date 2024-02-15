dadb3e05982e2b0d3fb12b48f27677744b63947f
Eray Ocak <erayeminocak@gmail.com>
2024-02-15 21:07:50 +0300

    Make armPreset and arm global

--- src/main/java/com/teamcelestial/commands/ArmControlCommand.kt
+++ src/main/java/com/teamcelestial/commands/ArmControlCommand.kt
@@ -3,18 +3,19 @@ package com.teamcelestial.commands
 import com.teamcelestial.subsystems.Arm
 import com.teamcelestial.system.arm.ArmPresetData
 import edu.wpi.first.wpilibj2.command.Command
+
+private val armPreset = ArmPresetData(
+    defaultTheta = 90.0, //TODO: The default theta, target angle when robot starts
+    absZeroPointDegrees = 0.14 * 360.0 //TODO: The absolute zero point of the arm in degrees. Must be parallel to ground.
+)
+
+val arm = Arm(
+    armPresetData = armPreset
+)
+
 class ArmControlCommand(
     private val supplier: () -> Double
 ) : Command() {
-
-    private val armPreset = ArmPresetData(
-        defaultTheta = 90.0, //TODO: The default theta, target angle when robot starts
-        absZeroPointDegrees = 0.14 * 360.0 //TODO: The absolute zero point of the arm in degrees. Must be parallel to ground.
-    )
-
-    val arm = Arm(
-        armPresetData = armPreset
-    )
 
     init {
         addRequirements(arm)
