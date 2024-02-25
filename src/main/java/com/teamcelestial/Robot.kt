6264b75c078d20588fced07ff883768f5d3b5b65
Eray Ocak <erayeminocak@gmail.com>
2024-02-25 20:20:22 +0300

    Update absZeroPointDegrees

--- src/main/java/com/teamcelestial/Robot.kt
+++ src/main/java/com/teamcelestial/Robot.kt
@@ -20,7 +20,7 @@ object Robot : TimedRobot() {
 
     private val rotatorPreset = RotatorPresetData(
         defaultTheta = 90.0, //TODO: The default theta, target angle when robot starts
-        absZeroPointDegrees = 0.29 //TODO: The absolute zero point of the arm in encoder units. Must be parallel to arm.
+        absZeroPointDegrees = 102.0 //TODO: The absolute zero point of the arm in encoder units. Must be parallel to arm.
     )
 
     private val rotator = Rotator(
