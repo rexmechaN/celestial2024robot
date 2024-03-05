5b75b30de1c56a2625fba560c6942a3e51b2d980
Eray Ocak <erayeminocak@gmail.com>
2024-03-05 11:25:42 +0300

    Update RelativeShooterTarget

--- src/main/java/com/teamcelestial/system/shooter/RelativeShooterTarget.kt
+++ src/main/java/com/teamcelestial/system/shooter/RelativeShooterTarget.kt
@@ -1,6 +1,9 @@
 package com.teamcelestial.system.shooter
 
+import com.teamcelestial.math.util.toRadians
 import com.teamcelestial.subsystems.Shooter
+import com.teamcelestial.subsystems.ShooterAssembly
+import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -8,12 +11,12 @@ class RelativeShooterTarget(
     private val y: Double,
     private val z: Double,
     private val theta: Double,
+    private val offsetHeight: Double = 0.0,
 ): ShooterTarget {
-    @Suppress("UNREACHABLE_CODE")
     override fun getTargetDistanceAndHeightPair(shooter: Shooter): Pair<Double, Double> {
-        val shooterTheta: Double = TODO()
-        val height = z * sin(shooterTheta) + y * cos(shooterTheta)
-        val distance = z * cos(shooterTheta) - y * sin(shooterTheta)
+        val shooterThetaRadians: Double = ShooterAssembly.getShooterAbsTheta().toRadians()
+        val height = z * sin(shooterThetaRadians) + y * cos(shooterThetaRadians) + offsetHeight
+        val distance = z * cos(shooterThetaRadians) - y * sin(shooterThetaRadians)
         return Pair(distance, height)
     }
 }
