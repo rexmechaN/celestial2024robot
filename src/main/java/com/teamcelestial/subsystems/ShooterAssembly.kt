7a8fed40ec794ff381e7bfb424c7b8c4a35399eb
Eray Ocak <erayeminocak@gmail.com>
2024-03-07 00:19:23 +0300

    Implement ShooterAssembly.setShooterTarget

--- src/main/java/com/teamcelestial/subsystems/ShooterAssembly.kt
+++ src/main/java/com/teamcelestial/subsystems/ShooterAssembly.kt
@@ -1,17 +1,26 @@
 package com.teamcelestial.subsystems
 
+import com.teamcelestial.system.shooter.ShooterTarget
+
 object ShooterAssembly {
     private lateinit var arm: Arm
     private lateinit var rotator: Rotator
+    private lateinit var shooter: Shooter
 
-    fun initializeWithSubsystems(arm: Arm, rotator: Rotator) {
+    fun initializeWithSubsystems(arm: Arm, rotator: Rotator, shooter: Shooter) {
         this.arm = arm
         this.rotator = rotator
+        this.shooter = shooter
     }
 
     fun getShooterAbsTheta(): Double {
         val link1Theta = 270.0 - arm.getTheta()
         val link2Theta = -(rotator.getTheta() - rotator.rotatorPreset.defaultTheta)
         return link1Theta + link2Theta
+    }
+
+    fun setShooterTarget(shooterTarget: ShooterTarget) {
+        val result = shooterTarget.getTargetDistanceAndHeightPair(shooter)
+        shooter.start(result.first, result.second)
     }
 }
