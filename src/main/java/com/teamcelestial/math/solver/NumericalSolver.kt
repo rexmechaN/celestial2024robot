57d10ee35ef0d9e5ca183d7971adf5111ba50050
Eray Ocak <erayeminocak@gmail.com>
2024-03-13 18:08:12 +0300

    Add NumericalSolverMode

--- src/main/java/com/teamcelestial/math/solver/NumericalSolver.kt
+++ src/main/java/com/teamcelestial/math/solver/NumericalSolver.kt
@@ -1,21 +1,24 @@
 package com.teamcelestial.math.solver
 
 import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 class NumericalSolver(
     private val range: IntRange,
     private val step: Double,
     private val test: ((Double) -> Double),
 ) {
-    fun solveFor(target: Double): SolverResult {
+    fun solveFor(target: Double, solverMode: NumericalSolverMode): SolverResult {
         var current: Double = range.first.toDouble()
         var error: Double = Double.MAX_VALUE
         var best: Double = current
         var bestValue: Double = Double.MAX_VALUE
         while (current <= range.last) {
             test(current).let {value ->
-                (value - target).absoluteValue.let {
-                    if (it < error) {
+                (value - target).let {
+                    if(solverMode == NumericalSolverMode.A_PLUS_PARABOLIC_MINIMUM && error != Double.MAX_VALUE && error > 0 && it <= 0) return SolverResult(current, value)
+                    if(solverMode == NumericalSolverMode.A_PLUS_PARABOLIC_MAXIMUM && error != Double.MAX_VALUE && error < 0 && it >= 0) return SolverResult(current, value)
+                    if (it.absoluteValue < error.absoluteValue) {
                         best = current
                         bestValue = value
                         error = it
@@ -26,4 +29,10 @@ class NumericalSolver(
         }
         return SolverResult(best, bestValue)
     }
+}
+
+enum class NumericalSolverMode {
+    A_PLUS_PARABOLIC_MINIMUM,
+    A_PLUS_PARABOLIC_MAXIMUM,
+    BEST_RESULT,
 }
