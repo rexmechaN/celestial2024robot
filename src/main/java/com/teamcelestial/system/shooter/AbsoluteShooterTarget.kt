94846a35b528b4a2e9b4194771291312135ecb51
Eray Ocak <erayeminocak@gmail.com>
2024-03-13 12:07:28 +0300

    Add dynamic height to AbsoluteShooterTarget

--- src/main/java/com/teamcelestial/system/shooter/AbsoluteShooterTarget.kt
+++ src/main/java/com/teamcelestial/system/shooter/AbsoluteShooterTarget.kt
@@ -1,12 +1,26 @@
 package com.teamcelestial.system.shooter
 
+import com.teamcelestial.math.util.toRadians
 import com.teamcelestial.subsystems.Shooter
+import com.teamcelestial.subsystems.ShooterAssembly
+import kotlin.math.absoluteValue
+import kotlin.math.sin
 
+const val LINK1_LENGTH = 0.30
+const val LINK2_LENGTH = 0.10
 class AbsoluteShooterTarget(
     private val distance: Double,
     private val height: Double
 ): ShooterTarget {
     override fun getTargetDistanceAndHeightPair(shooter: Shooter): Pair<Double, Double> {
-        return Pair(distance, height)
+        val shooterThetaRadians: Double = ShooterAssembly.getShooterAbsTheta().toRadians()
+        val link1ThetaRadians = ShooterAssembly.getLink1Theta().toRadians()
+        val link1AbsHeight = LINK1_LENGTH * sin(link1ThetaRadians).absoluteValue
+        val link2AbsHeight = LINK2_LENGTH * sin(shooterThetaRadians).absoluteValue
+        return Pair(distance, height - (link1AbsHeight + link2AbsHeight))
+    }
+
+    override fun toString(): String {
+        return "<AbsoluteShooterTarget Distance: $distance, Height: $height>"
     }
 }
