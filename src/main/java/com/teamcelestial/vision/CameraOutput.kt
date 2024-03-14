b989035728aa0894a3e91e074603c9df0cce7839
Cem Baykara <cm0bykr@gmail.com>
2024-03-14 10:18:37 +0300

    Update CameraOutput.kt

--- src/main/java/com/teamcelestial/vision/CameraOutput.kt
+++ src/main/java/com/teamcelestial/vision/CameraOutput.kt
@@ -2,12 +2,10 @@ package com.teamcelestial.vision
 
 import edu.wpi.first.math.geometry.Transform3d
 import org.photonvision.PhotonCamera
+import org.photonvision.targeting.PhotonTrackedTarget
 
 class CameraOutput(cameraName: String?) {
     private val camera = PhotonCamera(cameraName)
-    val bestTarget: Transform3d?
-        get() = camera.latestResult.bestTarget?.bestCameraToTarget
-
-    val allTargets: List<Transform3d>
-        get() = camera.latestResult.getTargets().map { it.bestCameraToTarget }
+    val bestTarget: PhotonTrackedTarget?
+        get() = camera.latestResult?.bestTarget
 }
